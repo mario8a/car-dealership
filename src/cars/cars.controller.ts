f5402c52a8b18adb69bbe617542a9a64ccc73eaf
Mario Ochoa <mario8ato@hotmail.com>
2024-10-25 21:12:50 -0600

    Add methods

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,50 @@
+import { CarsService } from './cars.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+// Un controlador va escuchar las solicitudes del cliente (postman) y va emitir una respuesa
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get()
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    console.log('vodu', body);
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(
+    @Param('id', ParseIntPipe)
+    @Body()
+    body: any,
+  ) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'delete',
+      id,
+    };
+  }
+}
